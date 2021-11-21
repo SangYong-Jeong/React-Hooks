33ea56434d1daba270f291c1ff1bec7e375b351c
SangYong-Jeong <mmaf@naver.com>
2021-11-21 15:52:51 +0900

    react-hooks study

---
+++ Chapter8-Hooks/8.2Info.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+
+const Info = () => {
+  const [name, setName] = useState('');
+  const [nickname, setNickname] = useState('');
+  useEffect(() => {
+    console.log('렌더링이 완료되었습니다!');
+    console.log({
+      name,
+      nickname,
+    });
+  });
+  const onChangeName = (e) => {
+    setName(e.target.value);
+  };
+  const onChangeNickname = (e) => {
+    setNickname(e.target.value);
+  };
+  return (
+    <div>
+      <div>
+        <input value={name} onChange={onChangeName} />
+        <input value={nickname} onChange={onChangeNickname} />
+      </div>
+      <div>
+        <div>
+          <b>이름:</b> {name}
+        </div>
+        <div>
+          <b>닉네임:</b> {nickname}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Info;
+
+// * useEffect는 리액트 컴포넌트가 렌더링될 때마다 특정 작업을 수행하도록 설정할 수 있는 Hooks 클래스형 컴포넌트의 componentDidMount와 componentDidUpdate를 합친 형태로 보아도 무방하다.
